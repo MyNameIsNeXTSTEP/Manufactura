71103f9d72f4879112df98307d8c4ddb5b9f069e
MyNameIsNeXTSTEP <madmath@bk.ru>
2026-02-10 20:49:48 +0300

    Initial monorepo structure

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "**/dist/**",
+      "**/build/**",
+      "**/node_modules/**"
+    ]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended
+];
